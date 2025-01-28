6df71c495951e9d9927b7bdca3587345e4ec873d
Kyuqdev <shulhan.nazarii@gmail.com>
2025-01-28 17:36:45 +0200

    file upload trial

--- compiler.py
+++ compiler.py
@@ -1,17 +1,18 @@
 import json
 class API:
     def __init__(self, username="test"):
-        #opens the set user file for "username" being the user token
-        #for test the token is "test", file is the json opened
-        with open(f"users/{username}/{username}.json", "r") as f:
+        self.username = username
+    def read_data(self, file="test"):
+        with open(f"users/{self.username}/{file}.json", "r") as f:
             #loads the text inside the user json file into "userfile"
-            userfile = json.load(f)
-        #print(userfile)
-
-class data_compiler:
-    #cl_object is an object of the API class which is gonna be compiled into plain html file text
-    def __init__(self, cl_object):
-        pass
+            try:
+                userfile = json.load(f)
+            except json.decoder.JSONDecodeError:
+                userfile = None
+        return userfile
+    def write_data(self, file="hi", userfile="Corrupted/no data"):
+        with open(f"users/{self.username}/{file}.json", "w") as f:
+            f.write(json.dumps(userfile))
 
 if __name__ == "__main__":
-    API()
+    cl = API()
